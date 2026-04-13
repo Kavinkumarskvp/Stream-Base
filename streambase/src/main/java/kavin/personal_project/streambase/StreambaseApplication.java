6d7179532821f2a999fc02008b993fc6b96c83e9
kavinkumar <kavinkumarskvp@gmail.com>
2026-04-13 12:26:40 +0530

    Initial Commit (Spring Project with Spring Data JPA, Flyway, Spring wev, Lombok, Postgres, Docker dependencies

---
+++ streambase/src/main/java/kavin/personal_project/streambase/StreambaseApplication.java
@@ -0,0 +1,13 @@
+package kavin.personal_project.streambase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreambaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreambaseApplication.class, args);
+	}
+
+}
---
+++ streambase/src/test/java/kavin/personal_project/streambase/StreambaseApplicationTests.java
@@ -0,0 +1,13 @@
+package kavin.personal_project.streambase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreambaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
